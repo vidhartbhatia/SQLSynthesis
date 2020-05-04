824adbec48ed4fb24c351ee7a4abc3d150b5183a
Medha <medhapotluri@gmail.com>
2020-05-03 22:37:24 -0400

    debugging where

--- synthesis.py
+++ synthesis.py
@@ -22,31 +22,31 @@ def cellNotEqual(c1, c2):
     return If(cellType(c1) == StringVal('int'), And(And(And(And(cellType(c1) == cellType(c2),  cellInt(c1) != cellInt(c2)), cellReal(c1) == cellReal(c2)), cellBool(c1) == cellBool(c2)), cellString(c1) == cellString(c2)), \
     If(cellType(c1) == StringVal('real'), And(And(And(And(cellType(c1) == cellType(c2),  cellInt(c1) == cellInt(c2)), cellReal(c1) != cellReal(c2)), cellBool(c1) == cellBool(c2)), cellString(c1) == cellString(c2)), \
     If(cellType(c1) == StringVal('bool'), And(And(And(And(cellType(c1) == cellType(c2),  cellInt(c1) == cellInt(c2)), cellReal(c1) == cellReal(c2)), cellBool(c1) != cellBool(c2)), cellString(c1) == cellString(c2)), \
-    If(cellType(c1) == StringVal('string'), And(And(And(And(cellType(c1) == cellType(c2),  cellInt(c1) == cellInt(c2)), cellReal(c1) == cellReal(c2)), cellBool(c1) == cellBool(c2)), cellString(c1) != cellString(c2))))))
+    If(cellType(c1) == StringVal('string'), And(And(And(And(cellType(c1) == cellType(c2),  cellInt(c1) == cellInt(c2)), cellReal(c1) == cellReal(c2)), cellBool(c1) == cellBool(c2)), cellString(c1) != cellString(c2)), False))))
 
 def cellLessThan(c1, c2):
     return If(cellType(c1) == StringVal('int'), And(And(And(And(cellType(c1) == cellType(c2),  cellInt(c1) < cellInt(c2)), cellReal(c1) == cellReal(c2)), cellBool(c1) == cellBool(c2)), cellString(c1) == cellString(c2)), \
     If(cellType(c1) == StringVal('real'), And(And(And(And(cellType(c1) == cellType(c2),  cellInt(c1) == cellInt(c2)), cellReal(c1) < cellReal(c2)), cellBool(c1) == cellBool(c2)), cellString(c1) == cellString(c2)), \
-    If(cellType(c1) == StringVal('bool'), And(And(And(And(cellType(c1) == cellType(c2),  cellInt(c1) == cellInt(c2)), cellReal(c1) == cellReal(c2)), cellBool(c1) < cellBool(c2)), cellString(c1) == cellString(c2)), \
-    If(cellType(c1) == StringVal('string'), And(And(And(And(cellType(c1) == cellType(c2),  cellInt(c1) == cellInt(c2)), cellReal(c1) == cellReal(c2)), cellBool(c1) == cellBool(c2)), cellString(c1) < cellString(c2))))))
+    If(cellType(c1) == StringVal('bool'), And(And(And(And(cellType(c1) == cellType(c2),  cellInt(c1) == cellInt(c2)), cellReal(c1) == cellReal(c2)), False), cellString(c1) == cellString(c2)), \
+    If(cellType(c1) == StringVal('string'), And(And(And(And(cellType(c1) == cellType(c2),  cellInt(c1) == cellInt(c2)), cellReal(c1) == cellReal(c2)), cellBool(c1) == cellBool(c2)), cellString(c1) < cellString(c2)), False))))
 
 def cellGreaterThan(c1, c2):
     return If(cellType(c1) == StringVal('int'), And(And(And(And(cellType(c1) == cellType(c2),  cellInt(c1) > cellInt(c2)), cellReal(c1) == cellReal(c2)), cellBool(c1) == cellBool(c2)), cellString(c1) == cellString(c2)), \
     If(cellType(c1) == StringVal('real'), And(And(And(And(cellType(c1) == cellType(c2),  cellInt(c1) == cellInt(c2)), cellReal(c1) > cellReal(c2)), cellBool(c1) == cellBool(c2)), cellString(c1) == cellString(c2)), \
-    If(cellType(c1) == StringVal('bool'), And(And(And(And(cellType(c1) == cellType(c2),  cellInt(c1) == cellInt(c2)), cellReal(c1) == cellReal(c2)), cellBool(c1) > cellBool(c2)), cellString(c1) == cellString(c2)), \
-    If(cellType(c1) == StringVal('string'), And(And(And(And(cellType(c1) == cellType(c2),  cellInt(c1) == cellInt(c2)), cellReal(c1) == cellReal(c2)), cellBool(c1) == cellBool(c2)), cellString(c1) > cellString(c2))))))
+    If(cellType(c1) == StringVal('bool'), And(And(And(And(cellType(c1) == cellType(c2),  cellInt(c1) == cellInt(c2)), cellReal(c1) == cellReal(c2)), False), cellString(c1) == cellString(c2)), \
+    If(cellType(c1) == StringVal('string'), And(And(And(And(cellType(c1) == cellType(c2),  cellInt(c1) == cellInt(c2)), cellReal(c1) == cellReal(c2)), cellBool(c1) == cellBool(c2)), cellString(c1) > cellString(c2)), False))))
 
 def cellLTE(c1, c2):
     return If(cellType(c1) == StringVal('int'), And(And(And(And(cellType(c1) == cellType(c2),  cellInt(c1) <= cellInt(c2)), cellReal(c1) == cellReal(c2)), cellBool(c1) == cellBool(c2)), cellString(c1) == cellString(c2)), \
     If(cellType(c1) == StringVal('real'), And(And(And(And(cellType(c1) == cellType(c2),  cellInt(c1) == cellInt(c2)), cellReal(c1) <= cellReal(c2)), cellBool(c1) == cellBool(c2)), cellString(c1) == cellString(c2)), \
-    If(cellType(c1) == StringVal('bool'), And(And(And(And(cellType(c1) == cellType(c2),  cellInt(c1) == cellInt(c2)), cellReal(c1) == cellReal(c2)), cellBool(c1) <= cellBool(c2)), cellString(c1) == cellString(c2)), \
-    If(cellType(c1) == StringVal('string'), And(And(And(And(cellType(c1) == cellType(c2),  cellInt(c1) == cellInt(c2)), cellReal(c1) == cellReal(c2)), cellBool(c1) == cellBool(c2)), cellString(c1) <= cellString(c2))))))
+    If(cellType(c1) == StringVal('bool'), And(And(And(And(cellType(c1) == cellType(c2),  cellInt(c1) == cellInt(c2)), cellReal(c1) == cellReal(c2)), False), cellString(c1) == cellString(c2)), \
+    If(cellType(c1) == StringVal('string'), And(And(And(And(cellType(c1) == cellType(c2),  cellInt(c1) == cellInt(c2)), cellReal(c1) == cellReal(c2)), cellBool(c1) == cellBool(c2)), cellString(c1) <= cellString(c2)), False))))
 
 def cellGTE(c1, c2):
     return If(cellType(c1) == StringVal('int'), And(And(And(And(cellType(c1) == cellType(c2),  cellInt(c1) >= cellInt(c2)), cellReal(c1) == cellReal(c2)), cellBool(c1) == cellBool(c2)), cellString(c1) == cellString(c2)), \
     If(cellType(c1) == StringVal('real'), And(And(And(And(cellType(c1) == cellType(c2),  cellInt(c1) == cellInt(c2)), cellReal(c1) >= cellReal(c2)), cellBool(c1) == cellBool(c2)), cellString(c1) == cellString(c2)), \
-    If(cellType(c1) == StringVal('bool'), And(And(And(And(cellType(c1) == cellType(c2),  cellInt(c1) == cellInt(c2)), cellReal(c1) == cellReal(c2)), cellBool(c1) >= cellBool(c2)), cellString(c1) == cellString(c2)), \
-    If(cellType(c1) == StringVal('string'), And(And(And(And(cellType(c1) == cellType(c2),  cellInt(c1) == cellInt(c2)), cellReal(c1) == cellReal(c2)), cellBool(c1) == cellBool(c2)), cellString(c1) >= cellString(c2))))))
+    If(cellType(c1) == StringVal('bool'), And(And(And(And(cellType(c1) == cellType(c2),  cellInt(c1) == cellInt(c2)), cellReal(c1) == cellReal(c2)), False), cellString(c1) == cellString(c2)), \
+    If(cellType(c1) == StringVal('string'), And(And(And(And(cellType(c1) == cellType(c2),  cellInt(c1) == cellInt(c2)), cellReal(c1) == cellReal(c2)), cellBool(c1) == cellBool(c2)), cellString(c1) >= cellString(c2)), False))))
 
 def satisfies_where_helper(input_table, r, where_col_name, where_operator, where_constant):
     return If(where_operator == StringVal("="), cellEqual(input_table[where_col_name][r], where_constant), \
@@ -86,8 +86,8 @@ if __name__ == '__main__':
 
     age_rows = Array('age_rows', IntSort(), Cell)
     age_rows = Store(age_rows, 0, cell(StringVal('int'), 22, RealVal(0), False, StringVal('')))
-    age_rows = Store(age_rows, 1, cell(StringVal('int'), 22, RealVal(0), False, StringVal('')))
-    age_rows = Store(age_rows, 2, cell(StringVal('int'), 21, RealVal(0), False, StringVal('')))
+    age_rows = Store(age_rows, 1, cell(StringVal('int'), 23, RealVal(0), False, StringVal('')))
+    age_rows = Store(age_rows, 2, cell(StringVal('int'), 24, RealVal(0), False, StringVal('')))
     age_rows = Store(age_rows, 3, cell(StringVal('int'), 20, RealVal(0), False, StringVal('')))
     age_rows = Store(age_rows, 4, cell(StringVal('int'), 21, RealVal(0), False, StringVal('')))
 
@@ -95,14 +95,19 @@ if __name__ == '__main__':
     input_table = Store(input_table, StringVal('Name'), name_rows)
     input_table = Store(input_table, StringVal('Age'), age_rows)
 
-    # print(simplify(StringVal('Medha') == cellString(input_table[StringVal('Name')][0])))
+    output_age_rows = Array('output_age_rows', IntSort(), Cell)
+    age_rows = Store(age_rows, 0, cell(StringVal('int'), 22, RealVal(0), False, StringVal('')))
+    # age_rows = Store(age_rows, 1, cell(StringVal('int'), 23, RealVal(0), False, StringVal('')))
+    # age_rows = Store(age_rows, 2, cell(StringVal('int'), 24, RealVal(0), False, StringVal('')))
+    # age_rows = Store(age_rows, 3, cell(StringVal('int'), 21, RealVal(0), False, StringVal('')))
 
     output_table = Array('table', StringSort(), ArraySort(IntSort(), Cell))
-    output_table = Store(input_table, StringVal('Age'), age_rows)
+    output_table = Store(input_table, StringVal('Age'), output_age_rows)
 
     input_col_names = ['Name', 'Age']
     output_col_names = ['Age']
-    num_rows = 5
+    num_input_rows = 5
+    num_output_rows = 1
 
     solver = Solver()
 
@@ -122,24 +127,37 @@ if __name__ == '__main__':
     # WHERE domain constraints
     solver.add(Or([where_col_name == StringVal(input_col_name) for input_col_name in input_col_names]))
     solver.add(Or([where_operator == StringVal(operator) for operator in operators]))
-    solver.add(Or([cellEqual(where_constant, cell) for cell in input_table[where_col_name]]))
+    constraint = False
+    for r in range(num_input_rows):
+        constraint = Or(constraint, cellEqual(where_constant, input_table[where_col_name][r]))
+    solver.add(constraint)
+    
+    # solver.add(Or([cellEqual(where_constant, cell) for cell in input_table[where_col_name]]))
     
     # TODO: Optimization
     for c in range(len(output_col_names)):
-        for r in range(0, num_rows):
+        for r in range(num_input_rows):
             solver.add(satisfies_where(input_table, r, where_col_name, where_operator, where_constant, where_clause_missing))
             s = Int(f's{r}')
-            solver.add(And(s >= 0, s < num_rows))
+            solver.add(And(s >= 0, s < num_output_rows))
             input_col_name = select_col_names[c]
             output_col_name = StringVal(output_col_names[c])
             solver.add(cellEqual(input_table[input_col_name][r], output_table[output_col_name][s]))
-        for s in range(0, num_rows):
+        for s in range(num_output_rows):
             r = Int(f'r{s}')
-            solver.add(And(r >= 0, r < num_rows))
+            solver.add(And(r >= 0, r < num_input_rows))
             solver.add(satisfies_where(input_table, r, where_col_name, where_operator, where_constant, where_clause_missing))
             input_col_name = select_col_names[c]
             output_col_name = StringVal(output_col_names[c])
             solver.add(cellEqual(input_table[input_col_name][r], output_table[output_col_name][s]))
+
+    for i in range(num_input_rows-1):
+        for j in range(i+1, num_input_rows):
+            solver.add(Int(f'r{i}') != Int(f'r{j}'))
+
+    for i in range(num_output_rows-1):
+        for j in range(i+1, num_output_rows):
+            solver.add(Int(f's{i}') != Int(f's{j}'))
 
     sat = solver.check()
     # print(sat)
