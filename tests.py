427a203d9745ed40bcfcc83b533311c0f4f7787b
Medha <medhapotluri@gmail.com>
2020-05-11 23:58:04 -0400

    added tests from presentation

--- tests.py
+++ tests.py
@@ -1,6 +1,9 @@
 from z3 import *
 from synthesis import solve, Cell, cell
 
+# To run our tests, run python3 tests.py
+
+# INPUT TABLE
 name_rows = Array('name_rows', IntSort(), Cell)
 name_rows = Store(name_rows, 0, cell(StringVal('string'), 0, RealVal(0), StringVal('Vidhart')))
 name_rows = Store(name_rows, 1, cell(StringVal('string'), 0, RealVal(0), StringVal('Udit')))
@@ -31,6 +34,145 @@ input_col_names = ['Name', 'Age', 'Score']
 num_input_rows = 5
 
 if __name__ == "__main__":
+    # OUTPUT TABLE select
+    name_rows = Array('name_rows', IntSort(), Cell)
+    name_rows = Store(name_rows, 0, cell(StringVal('string'), 0, RealVal(0), StringVal('Vidhart')))
+    name_rows = Store(name_rows, 1, cell(StringVal('string'), 0, RealVal(0), StringVal('Udit')))
+    name_rows = Store(name_rows, 2, cell(StringVal('string'), 0, RealVal(0), StringVal('Ebru')))
+    name_rows = Store(name_rows, 3, cell(StringVal('string'), 0, RealVal(0), StringVal('Jeremy')))
+    name_rows = Store(name_rows, 4, cell(StringVal('string'), 0, RealVal(0), StringVal('Medha')))
+
+    age_rows = Array('age_rows', IntSort(), Cell)
+    age_rows = Store(age_rows, 0, cell(StringVal('int'), 19, RealVal(19), StringVal('')))
+    age_rows = Store(age_rows, 1, cell(StringVal('int'), 20, RealVal(20), StringVal('')))
+    age_rows = Store(age_rows, 2, cell(StringVal('int'), 21, RealVal(21), StringVal('')))
+    age_rows = Store(age_rows, 3, cell(StringVal('int'), 21, RealVal(21), StringVal('')))
+    age_rows = Store(age_rows, 4, cell(StringVal('int'), 22, RealVal(22), StringVal('')))
+
+    output_table = Array('output', StringSort(), ArraySort(IntSort(), Cell))
+    output_table = Store(output_table, StringVal('NAME'), name_rows)
+    output_table = Store(output_table, StringVal('AGE'), age_rows)
+    output_col_names = ['NAME', 'AGE']
+    num_output_rows = 5
+
+    print('Test SELECT')
+    solve(input_table, input_col_names, num_input_rows, output_table, output_col_names, num_output_rows)
+    print('')
+    # result: "SELECT Name AS NAME, Age AS AGE FROM input_table"
+    # time: 0.24 s
+
+    # OUTPUT TABLE where
+    name_rows = Array('name_rows', IntSort(), Cell)
+    name_rows = Store(name_rows, 0, cell(StringVal('string'), 0, RealVal(0), StringVal('Vidhart')))
+    name_rows = Store(name_rows, 1, cell(StringVal('string'), 0, RealVal(0), StringVal('Udit')))
+    name_rows = Store(name_rows, 2, cell(StringVal('string'), 0, RealVal(0), StringVal('Jeremy')))
+
+    score_rows = Array('score_rows', IntSort(), Cell)
+    score_rows = Store(score_rows, 0, cell(StringVal('real'), 0, RealVal(75.0), StringVal('')))
+    score_rows = Store(score_rows, 1, cell(StringVal('real'), 0, RealVal(50.0), StringVal('')))
+    score_rows = Store(score_rows, 2, cell(StringVal('real'), 0, RealVal(100.0), StringVal('')))
+
+    output_table = Array('output_table', StringSort(), ArraySort(IntSort(), Cell))
+    output_table = Store(output_table, StringVal('Passing students'), name_rows)
+    output_table = Store(output_table, StringVal('Score'), score_rows)
+    output_col_names = ['Passing students', 'Score']
+    num_output_rows = 3
+
+    print('Test WHERE')
+    solve(input_table, input_col_names, num_input_rows, output_table, output_col_names, num_output_rows)
+    print('')
+    # result: "SELECT Name AS Passing students, Score AS Score FROM input_table WHERE Score >= 50"
+    # time: 0.30 s
+
+
+    # OUTPUT TABLE avg
+    output_score_rows = Array('output_score_rows', IntSort(), Cell)
+    output_score_rows = Store(output_score_rows, 0, cell(StringVal('real'), 0, RealVal(49.48), StringVal('')))
+
+    output_table = Array('output_table', StringSort(), ArraySort(IntSort(), Cell))
+    output_table = Store(output_table, StringVal('Avg Score'), output_score_rows)
+    output_col_names = ['Avg Score']
+    num_output_rows = 1
+
+    print('Test AVG')
+    solve(input_table, input_col_names, num_input_rows, output_table, output_col_names, num_output_rows)
+    print('')
+    # result: "SELECT AVG(Score) AS Avg Score FROM input_table"
+    # time: 36.78 s
+
+
+    # OUTPUT TABLE count
+    output_age_rows = Array('output_age_rows', IntSort(), Cell)
+    output_age_rows = Store(output_age_rows, 0, cell(StringVal('int'), 19, RealVal(19), StringVal('')))
+    output_age_rows = Store(output_age_rows, 1, cell(StringVal('int'), 20, RealVal(20), StringVal('')))
+    output_age_rows = Store(output_age_rows, 2, cell(StringVal('int'), 21, RealVal(21), StringVal('')))
+    output_age_rows = Store(output_age_rows, 3, cell(StringVal('int'), 22, RealVal(22), StringVal('')))
+
+    output_count_rows = Array('output_count_rows', IntSort(), Cell)
+    output_count_rows = Store(output_count_rows, 0, cell(StringVal('int'), 1, RealVal(1), StringVal('')))
+    output_count_rows = Store(output_count_rows, 1, cell(StringVal('int'), 1, RealVal(1), StringVal('')))
+    output_count_rows = Store(output_count_rows, 2, cell(StringVal('int'), 2, RealVal(2), StringVal('')))
+    output_count_rows = Store(output_count_rows, 3, cell(StringVal('int'), 1, RealVal(1), StringVal('')))
+
+    output_table = Array('output_table', StringSort(), ArraySort(IntSort(), Cell))
+    output_table = Store(output_table, StringVal('Age'), output_age_rows)
+    output_table = Store(output_table, StringVal('Count'), output_count_rows)
+    output_col_names = ['Age', 'Count']
+    num_output_rows = 4
+
+    print('Test COUNT')
+    solve(input_table, input_col_names, num_input_rows, output_table, output_col_names, num_output_rows)
+    print('')
+    # result: "SELECT MIN(Age) AS Age, COUNT(Age) AS Count FROM input_table GROUP BY Age"
+    # time: 39.16 s
+
+
+    # OUTPUT TABLE having 
+    output_age_rows = Array('output_age_rows', IntSort(), Cell)
+    output_age_rows = Store(output_age_rows, 0, cell(StringVal('int'), 19, RealVal(19), StringVal('')))
+    output_age_rows = Store(output_age_rows, 1, cell(StringVal('int'), 21, RealVal(21), StringVal('')))
+
+    output_score_rows = Array('output_score_rows', IntSort(), Cell)
+    output_score_rows = Store(output_score_rows, 0, cell(StringVal('real'), 0, RealVal(75.0), StringVal('')))
+    output_score_rows = Store(output_score_rows, 1, cell(StringVal('real'), 0, RealVal(109.9), StringVal('')))
+
+    output_table = Array('output_table', StringSort(), ArraySort(IntSort(), Cell))
+    output_table = Store(output_table, StringVal('Age'), output_age_rows)
+    output_table = Store(output_table, StringVal('Sum of Scores'), output_score_rows)
+    output_col_names = ['Age', 'Sum of Scores']
+    num_output_rows = 2
+
+    print('Test HAVING')
+    solve(input_table, input_col_names, num_input_rows, output_table, output_col_names, num_output_rows)
+    print('')
+    # result: "SELECT Age AS Age, SUM(Score) AS Sum of Scores FROM input_table WHERE Name >= "Ebru" GROUP BY Age HAVING SUM(Score) >= 75"
+    # time: 
+
+
+    # OUTPUT TABLE having 2
+    output_age_rows = Array('output_age_rows', IntSort(), Cell)
+    output_age_rows = Store(output_age_rows, 0, cell(StringVal('int'), 21, RealVal(21), StringVal('')))
+    output_age_rows = Store(output_age_rows, 1, cell(StringVal('int'), 22, RealVal(22), StringVal('')))
+
+    output_score_rows = Array('output_score_rows', IntSort(), Cell)
+    output_score_rows = Store(output_score_rows, 0, cell(StringVal('real'), 0, RealVal(100), StringVal('')))
+    output_score_rows = Store(output_score_rows, 1, cell(StringVal('real'), 0, RealVal(50.0), StringVal('')))
+
+    output_table = Array('output_table', StringSort(), ArraySort(IntSort(), Cell))
+    output_table = Store(output_table, StringVal('Age'), output_age_rows)
+    output_table = Store(output_table, StringVal('Max Score'), output_score_rows)
+    output_col_names = ['Age', 'Max Score']
+    num_output_rows = 2
+
+    print('Test having 1')
+    solve(input_table, input_col_names, num_input_rows, output_table, output_col_names, num_output_rows)
+    print('')
+
+# Test having 2
+# Query generated:
+# "SELECT Age AS Age, MAX(Score) AS Max Score FROM input_table GROUP BY Age HAVING MAX(Score) != 99/10"
+# with having in time 83.11 
+
      # OUTPUT TABLE avg score
     output_age_rows = Array('output_age_rows', IntSort(), Cell)
     output_age_rows = Store(output_age_rows, 0, cell(StringVal('int'), 22, RealVal(22), StringVal('')))
@@ -44,30 +186,6 @@ if __name__ == "__main__":
     solve(input_table, input_col_names, num_input_rows, output_table, output_col_names, num_output_rows)
     print('')
 
-    # # OUTPUT TABLE having 1
-    # output_age_rows = Array('output_age_rows', IntSort(), Cell)
-    # output_age_rows = Store(output_age_rows, 0, cell(StringVal('int'), 21, RealVal(21), StringVal('')))
-    # output_age_rows = Store(output_age_rows, 1, cell(StringVal('int'), 22, RealVal(22), StringVal('')))
-
-    # output_score_rows = Array('output_score_rows', IntSort(), Cell)
-    # output_score_rows = Store(output_score_rows, 0, cell(StringVal('real'), 0, RealVal(100), StringVal('')))
-    # output_score_rows = Store(output_score_rows, 1, cell(StringVal('real'), 0, RealVal(50.0), StringVal('')))
-
-    # output_table = Array('output_table', StringSort(), ArraySort(IntSort(), Cell))
-    # output_table = Store(output_table, StringVal('Age'), output_age_rows)
-    # output_table = Store(output_table, StringVal('Max Score'), output_score_rows)
-    # output_col_names = ['Age', 'Max Score']
-    # num_output_rows = 2
-
-    # print('Test having 1')
-    # solve(input_table, input_col_names, num_input_rows, output_table, output_col_names, num_output_rows)
-    # print('')
-
-#     Test having 1
-# Query generated:
-# "SELECT Age AS Age, MAX(Score) AS Max Score FROM input_table GROUP BY Age HAVING MAX(Score) != 99/10"
-# with having in time 83.11 
-
      # OUTPUT TABLE sum score
     output_age_rows = Array('output_age_rows', IntSort(), Cell)
     output_age_rows = Store(output_age_rows, 0, cell(StringVal('real'), 0, RealVal(222.4), StringVal('')))
@@ -80,13 +198,12 @@ if __name__ == "__main__":
     print('Test sum score')
     solve(input_table, input_col_names, num_input_rows, output_table, output_col_names, num_output_rows)
     print('')
-
+    
 
 # Test sum score
 # Query generated:
 # "SELECT SUM(Score) AS Sum_score FROM input_table WHERE Age >= 19"
 # with group by, in time 92.73
-
 
 
     # OUTPUT TABLE count 1
